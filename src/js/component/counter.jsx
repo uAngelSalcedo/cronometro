fa7533f844f6d61b5d954704df7df914a5a4aff3
uAngelSalcedo <162602493+uAngelSalcedo@users.noreply.github.com>
2024-06-18 21:11:38 +0000

    counter

--- src/js/component/counter.jsx
+++ src/js/component/counter.jsx
@@ -2,17 +2,18 @@ import React from "react";
 
 const Counter = (props) => {
 
-	const secondsPad = props.seconds.padStart(5, "0");
+	const secondsPad = props.seconds.padStart(6, "0");
 
 	const valores = secondsPad.split("");
 
 	return (
-		<div className="text-center">
-			<div className="" >{valores [0]}</div>
-			<div className="">{valores [1]}</div>
-			<div className="">{valores [2]}</div>
-			<div className="">{valores [3]}</div>
-			<div className="">{valores [4]}</div>
+		<div className="fs-1 d-flex justify-content-center row container-fluid bg-black text-light">
+			<div className="col-1 bg-white bg-opacity-10 mb-2 mt-2 mx-2 rounded border border-2 d-flex justify-content-center">{<i class="fa-solid fa-clock"></i> [0]}</div>
+			<div className="col-1 bg-white bg-opacity-10 mb-2 mt-2 mx-2 rounded border border-2 d-flex justify-content-center">{valores [1]}</div>
+			<div className="col-1 bg-white bg-opacity-10 mb-2 mt-2 mx-2 rounded border border-2 d-flex justify-content-center">{valores [2]}</div>
+			<div className="col-1 bg-white bg-opacity-10 mb-2 mt-2 mx-2 rounded border border-2 d-flex justify-content-center">{valores [3]}</div>
+			<div className="col-1 bg-white bg-opacity-10 mb-2 mt-2 mx-2 rounded border border-2 d-flex justify-content-center">{valores [4]}</div>
+			<div className="col-1 bg-white bg-opacity-10 mb-2 mt-2 mx-2 rounded border border-2 d-flex justify-content-center">{valores [5]}</div>
 		</div>
 	);
 };
